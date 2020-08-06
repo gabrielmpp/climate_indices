150e6b37ad55e299b57b627b21893db31842f614
gab <gabrielmpp2@gmail.com>
2020-08-06 20:15:44 +0100

    update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'Gabriel Perez',                   # Type in your name
   author_email = 'gabrielmpp2@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/gabrielmpp/climate_indices',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/gabrielmpp/climate_indices/archive/v0.21.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/gabrielmpp/climate_indices/archive/v0.4.tar.gz',    # I explain this later on
   keywords = ['climate', 'pandas'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
